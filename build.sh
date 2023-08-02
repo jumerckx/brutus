63aa6937db3fcdb5b0fa8d9e286e77bf87eecdb7
jumerckx <julesmerckx12@gmail.com>
2023-08-02 23:57:22 +0200

    reduce!

--- build.sh
+++ build.sh
@@ -6,9 +6,7 @@ rm -rf $rootpath/build
 mkdir $rootpath/build && cd $rootpath/build
 
 cmake .. -G Ninja \
-    -DMLIR_DIR="/home/jumerckx/julia/build/debug/usr/lib/cmake/mlir" \
-    -DLLVM_EXTERNAL_LIT="/home/jumerckx/julia/build/debug/usr/tools/lit/lit.py" \
-    -DJulia_EXECUTABLE="/home/jumerckx/julia/build/debug/julia" \
+    -DMLIR_DIR="/storage/jumerckx/llvm_build/lib/cmake/mlir" \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo
 ninja
 
